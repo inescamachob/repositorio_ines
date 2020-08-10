4a7bc1506983ffec950a9815905876d48ea10d9c
inescamachob <69019847+inescamachob@users.noreply.github.com>
2020-08-10 20:46:48 -0300

    loginnuevo

--- js/login.js
+++ js/login.js
@@ -2,5 +2,18 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-
 });
+
+function login() {
+var x = document.forms['formulario']['nombre'].value;
+if (x == '') {
+    alert('Introduzca Usuario')
+    return false
+}
+
+var y = document.forms['formulario']['contrasenia'].value;
+if (y == '') {
+    alert('Introduzca Contraseña')
+    return false
+}
+}
