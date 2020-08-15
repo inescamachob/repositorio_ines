13d730a1592c72785238dc5446fbe885613814f6
inescamachob <69019847+inescamachob@users.noreply.github.com>
2020-08-15 12:22:34 -0300

    cambio index por login

--- js/init.js
+++ js/init.js
@@ -43,5 +43,6 @@ var getJSONData = function(url){
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-});
+//document.addEventListener("DOMContentLoaded", () => {
+// window.location = 'logincopy.html'
+//});
--- js/login.js
+++ js/login.js
@@ -1,14 +1,26 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function(e) {
 });
 
 function login(){
+    var x = document.forms['formulario']['nombre'].value;
+    var error = document.getElementById('errorusuario');
+    if (x == '') {
+       return error.innerHTML = 'Introduzca Usuario';
+    }
+    var y = document.forms['formulario']['contrasenia'].value;
+    var errorcontrasenia = document.getElementById('errorcontrasenia');
+    if (y == '') {
+        return errorcontrasenia.innerHTML = 'Introduzca Contraseña';
+    }
+    }
+
+/*function login(){
 var x = document.forms['formulario']['nombre'].value;
 if (x == '') {
-    alert('Introduzca Usuario');
-    return false;
+   return error.innerHTML = 'Introduzca Usuario';
 }
 var y = document.forms['formulario']['contrasenia'].value;
 if (y == '') {
@@ -16,3 +28,9 @@ if (y == '') {
     return false;
 }
 }
+
+/*function validar(){
+    var parrafo = document.createElement('p');
+    parrafo.innerHTML = 'Introduzca Contraseña';
+    document.body.appendChild(parrafo);
+    }*/
